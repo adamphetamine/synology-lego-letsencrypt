8a971b8e4e839ad12e00dec40b48fd9b5f7fd5a1
adamphetamine <47110465+adamphetamine@users.noreply.github.com>
2022-11-01 16:13:33 +1100

    Update setup.sh
    added a bunch of echo commands for debugging

--- setup.sh
+++ setup.sh
@@ -7,28 +7,35 @@ echo Current version of Acme Lego is $lego_tag
 echo -----------------------------------
 
 # Download it and print the current directory to the user
-echo 'we are downloading to ' $PWD
+echo 'we are downloading Lego to ' $PWD
 wget https://github.com/go-acme/lego/releases/download/$lego_tag/lego_$lego_tag_linux_$(dpkg --print-architecture).tar.gz
 
 # original command = wget  https://github.com/gravitl/netmaker/releases/download/$netclient_tag/netclient-darwin
 
-# unpack the binaary
+# unpack the binary
+echo unpack the binary
 tar xvzf lego_$lego_tag_linux_$(dpkg --print-architecture).tar.gz 
 
 # create a new directory structure to hold the project
+echo create a new directory structure to hold the project
 sudo mkdir -p /usr/local/sbin
 
 # Move the binary into position, update permissions and check it launches...
+echo Move the binary into position
 sudo mv ./lego /usr/local/sbin
+echo update permissions
 sudo chown root:root /usr/local/sbin/lego
 # Make sure it works!
+echo check it launches...
 ./lego --version
 
-# get the custom.env to use for variables
+# get the custom.env to use with our variables
+echo get the custom.env to use with our variables
 wget https://raw.githubusercontent.com/adamphetamine/synology-lego-letsencrypt/main/custom.env
 
 
 # get the renewal script so we can renew our certificates
+echo get the renewal script so we can renew our certificates
 https://raw.githubusercontent.com/adamphetamine/synology-lego-letsencrypt/main/le-renew.sh
 chmod +x le-renew.sh
 
@@ -50,5 +57,10 @@ echo
 echo -----------------------------------
 echo Your setup WILL FAIL unless you complete this
 echo -----------------------------------
+echo
+echo update the variables
+echo run the command once to get your certs
+echo then test the move-certs script
+echo and set up a cron schedule for the le-renew script
 echo
 echo "all done. exiting"
